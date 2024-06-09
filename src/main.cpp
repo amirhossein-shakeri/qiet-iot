9979b50b464d9cdd5cba3e136315e9e431fdb693
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2024-06-09 20:06:53 +0330

    feat: complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ArduinoWebsockets.h>
+#include <WebSocketsClient.h>
 // #include <WiFiClientSecure.h>
 // #include <HTTPClient.h>
 #include <Adafruit_NeoPixel.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 // DS18b20
+#include <ArduinoJson.h>
 
-using namespace websockets;
+// using namespace websockets;
 
 TaskHandle_t NetworkTask;
 TaskHandle_t ThermostatTask;
@@ -22,8 +24,8 @@ void IdleHandler(void *pvParameters);
 void colorTransition(uint32_t targetColor, int transitionTime);
 void led(bool on, int delay_ms);
 void neo(uint8_t r, uint8_t g, uint8_t b, int delay_ms);
-void onWSMessage(WebsocketsMessage message);
-void onWSEvent(WebsocketsEvent event, String data);
+void onWSMessage(websockets::WebsocketsMessage message);
+void onWSEvent(websockets::WebsocketsEvent event, String data);
 
 #define LED 2
 #define NEO 22
@@ -37,11 +39,31 @@ DallasTemperature sensors(&oneWire);
 
 const char *ssid = "Xiaomi 11 Lite 5G NE";
 const char *password = "fibonachi";
-const char *ws_server_host = "iot.thor-electronics.ir";
-const uint16_t ws_server_port = 80;
-WebsocketsClient wsClient;
+const char *TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NjVhMjI3ZjFhODA4OGI2MTkxMDZmMyIsImNwdUlkIjoiNjY2NWEyMjdmMWE4MDg4YjYxOTEwNmYzIiwibmFtZSI6IlRoZXJtb3giLCJ1c2VySWQiOiI2MzI1N2UxOWIwMDI4MzJmODQ4ZmQ5ZjgiLCJncm91cElkIjoiNjY2NWExZjVmMWE4MDg4YjYxOTEwNmYxIiwidHlwZSI6IlRIRVJNT1NUQVQiLCJpc3MiOiJpb3QtY29yZS1hdXRoLXNlcnZlciIsImV4cCI6MTcyMDUzMDg4OSwiaWF0IjoxNzE3OTM4ODg5fQ.IpA6VxuWpUcuzduDqESJ5-ovn3fVg77jfaL8J1aPSmA";
+websockets::WebsocketsClient wsClient;
+WebSocketsClient wsC;
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  {
+  case WStype_ERROR:
+  case WStype_DISCONNECTED:
+    Serial.printf("[WebSocket] Disconnected!\n");
+    break;
+  case WStype_CONNECTED:
+    Serial.printf("[WebSocket] Connected to: %s\n", payload);
+    // Send a message to the server
+    wsC.sendTXT("Hello from ESP32!");
+    break;
+  case WStype_TEXT:
+    Serial.printf("[WebSocket] Received text: %s\n", payload);
+    break;
+  }
+}
 
 float TEMP_THRESHOLD = 32.0;
+float CURRENT_TEMP = -127.0;
 
 void setup()
 {
@@ -54,7 +76,7 @@ void setup()
   Serial.print("Connecting to WiFi ");
   Serial.print(ssid);
   WiFi.begin(ssid, password);
-  for (int i = 10; i > 0 && WiFi.status() != WL_CONNECTED; i--)
+  for (int i = 20; i > 0 && WiFi.status() != WL_CONNECTED; i--)
   {
     Serial.print(".");
     neo(100, 0, 125, 200);
@@ -66,16 +88,31 @@ void setup()
   Serial.println("Connecting to WS server...");
   wsClient.onMessage(onWSMessage);
   wsClient.onEvent(onWSEvent);
+  wsClient.setInsecure();
   // wsClient.connect("ws://iot.thor-electronics.ir/api/v1/control", 80);
-  bool connection = wsClient.connect(ws_server_host, ws_server_port, "/api/v1/control");
+  // bool connection = wsClient.connect("ws://iot.thor-electronics.ir/api/v1/control/");
+  // bool connection = wsClient.connectSecure("iot.thor-electronics.ir", 443, "/api/v1/control/");
+  // bool connection = wsClient.connect("iot.thor-electronics.ir", 80, "/api/v1/control/");
+  bool connection = wsClient.connect("192.168.34.105", 3993, "/api/v1/control/");
+  // bool connection = wsClient.connect("ws://echo.websocket.org");
+  // bool connection = wsClient.connect("ws://192.168.34.105:3993/api/v1/control/");
+  wsClient.setInsecure();
+  Serial.print("WS Connection: ");
   Serial.println(connection);
+
+  Serial.print("Connecting to WSC...");
+  // wsC.begin("echo.websocket.org", 80, "/");
+  // wsC.begin("iot.thor-electronics.ir", 80, "/api/v1/control/");
+  // wsC.begin("192.168.34.105", 3993, "/api/v1/control/");
+  wsC.onEvent(webSocketEvent);
+  wsC.setReconnectInterval(5000);
   // wsClient.send("{}");
   // wsClient.ping();
 
   // xTaskCreatePinnedToCore(NetworkHandler, "NetworkTask", 10000, NULL, 1, &NetworkTask, 0);
   // delay(500);
-  // xTaskCreatePinnedToCore(ThermostatHandler, "ThermostatTask", 10000, NULL, 1, &ThermostatTask, 1);
-  // delay(500);
+  xTaskCreatePinnedToCore(ThermostatHandler, "ThermostatTask", 10000, NULL, 1, &ThermostatTask, 1);
+  delay(500);
   // xTaskCreatePinnedToCore(IdleHandler, "IdleTask", 10000, NULL, 10, &IdleTask, 1);
   // delay(500);
 
@@ -86,6 +123,7 @@ void loop()
 {
   // Serial.println("Polling...");
   wsClient.poll();
+  // wsC.loop();
   // Serial.println("Polled!");
 }
 
@@ -94,10 +132,18 @@ void NetworkHandler(void *pvParameters)
   Serial.println("Initializing network task...");
   while (true)
   {
-    // wsClient.poll();
-    Serial.println("Network...");
-    // neo
-    delay(5000);
+    StaticJsonDocument<255> doc;
+    doc["ok"] = true;
+    doc["signal"] = "STATE_UPDATED";
+    doc["payload"]["temperature"] = CURRENT_TEMP;
+    doc["update"]["temperature"] = CURRENT_TEMP;
+    String jsonStr;
+    serializeJson(doc, jsonStr);
+    Serial.print("Sending temp update: ");
+    Serial.println(jsonStr);
+    wsClient.send(jsonStr);
+    led(true, 100);
+    led(false, 1000);
   }
 }
 
@@ -109,6 +155,7 @@ void ThermostatHandler(void *pvParameters)
   {
     sensors.requestTemperatures();
     float temperatureC = sensors.getTempCByIndex(0);
+    CURRENT_TEMP = temperatureC;
     Serial.print("Temp: ");
     Serial.print(temperatureC);
     Serial.print("C ");
@@ -131,6 +178,18 @@ void ThermostatHandler(void *pvParameters)
       colorTransition(pixels.Color(0, 50, 255), 3000);
     }
     // delay(1000);
+    StaticJsonDocument<255> doc;
+    doc["ok"] = true;
+    doc["signal"] = "STATE_UPDATED";
+    // doc["payload"]["temperature"] = temperatureC;
+    doc["update"]["temperature"] = temperatureC;
+    String jsonStr;
+    serializeJson(doc, jsonStr);
+    Serial.print("Sending temp update: ");
+    Serial.println(jsonStr);
+    wsClient.send(jsonStr);
+    led(true, 100);
+    led(false, 1000);
   }
 }
 
@@ -185,33 +244,61 @@ void neo(uint8_t r, uint8_t g, uint8_t b, int delay_ms)
   delay(delay_ms);
 }
 
-void onWSMessage(WebsocketsMessage message)
+void onWSMessage(websockets::WebsocketsMessage message)
 {
   Serial.print("Got WS Message: ");
   Serial.println(message.data());
+  StaticJsonDocument<255> doc;
+  DeserializationError error = deserializeJson(doc, message.data());
+  if (error)
+  {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+  float targetTemperature = doc["command"]["targetTemperature"];
+  if (targetTemperature)
+  {
+    TEMP_THRESHOLD = targetTemperature;
+    Serial.print("Target Temperature: ");
+    Serial.println(targetTemperature);
+  }
 }
 
-void onWSEvent(WebsocketsEvent event, String data)
+void onWSEvent(websockets::WebsocketsEvent event, String data)
 {
   Serial.print("WS event: ");
-  if (event == WebsocketsEvent::ConnectionOpened)
+  if (event == websockets::WebsocketsEvent::ConnectionOpened)
   {
     Serial.println("Connection Opened");
     neo(0, 255, 50, 1000);
-    // todo: send token
+    while (!wsClient.available())
+    {
+      neo(0, 0, 0, 500);
+      neo(250, 250, 250, 500);
+      Serial.print(".");
+    }
+    StaticJsonDocument<255> doc;
+    doc["signal"] = "AUTHENTICATE";
+    doc["payload"]["token"] = TOKEN;
+    String jsonStr;
+    serializeJson(doc, jsonStr);
+    Serial.print("Sending authenticate message: ");
+    Serial.println(jsonStr);
+    wsClient.send(jsonStr);
   }
-  else if (event == WebsocketsEvent::ConnectionClosed)
+  else if (event == websockets::WebsocketsEvent::ConnectionClosed)
   {
     neo(255, 0, 10, 1000);
     Serial.print("Connection Closed: ");
     Serial.println(wsClient.getCloseReason());
   }
-  else if (event == WebsocketsEvent::GotPing)
+  else if (event == websockets::WebsocketsEvent::GotPing)
   {
     neo(255, 255, 255, 100);
     Serial.println("Got a Ping!");
   }
-  else if (event == WebsocketsEvent::GotPong)
+  else if (event == websockets::WebsocketsEvent::GotPong)
   {
     neo(255, 255, 255, 100);
     Serial.println("Got a Pong!");
